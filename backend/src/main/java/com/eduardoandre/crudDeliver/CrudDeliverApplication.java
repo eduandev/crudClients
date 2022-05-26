3684e6221d29cada6f19eb743dbe7a2990c74899
Andev <andremoraes930@gmail.com>
2022-05-26 15:58:30 -0300

    First commit

---
+++ backend/src/main/java/com/eduardoandre/crudDeliver/CrudDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.eduardoandre.crudDeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudDeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eduardoandre/crudDeliver/CrudDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardoandre.crudDeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
