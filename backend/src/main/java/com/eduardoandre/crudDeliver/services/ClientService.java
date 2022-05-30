ceb8fcba55dbfa742db067e274bbbf97281ae84c
Andev <andremoraes930@gmail.com>
2022-05-30 16:59:32 -0300

    Created class DataBaseException, delete

--- backend/src/main/java/com/eduardoandre/crudDeliver/services/ClientService.java
+++ backend/src/main/java/com/eduardoandre/crudDeliver/services/ClientService.java
@@ -1,18 +1,21 @@
 package com.eduardoandre.crudDeliver.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.eduardoandre.crudDeliver.dto.ClientDTO;
 import com.eduardoandre.crudDeliver.entities.Client;
 import com.eduardoandre.crudDeliver.repositories.ClientRepository;
+import com.eduardoandre.crudDeliver.services.exceptions.DataBaseException;
 import com.eduardoandre.crudDeliver.services.exceptions.NotFoundEntityException;
 
 @Service
@@ -22,9 +25,9 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll() {
-		List<Client> list = repository.findAll();
-		return list.stream().map(c -> new ClientDTO(c)).collect(Collectors.toList());
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(c -> new ClientDTO(c));
 	}
 	
 	@Transactional(readOnly = true)
@@ -37,11 +40,7 @@ public class ClientService {
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
-		entity.setName(dto.getName());
-		entity.setCpf(dto.getCpf());
-		entity.setIncome(dto.getIncome());
-		entity.setBirthDate(dto.getBirthDate());
-		entity.setChildren(dto.getChildren());
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
 	}
@@ -50,11 +49,7 @@ public class ClientService {
 	public ClientDTO update(Long id, ClientDTO dto) {
 		try {
 			Client entity = repository.getReferenceById(id);
-			entity.setName(dto.getName());
-			entity.setCpf(dto.getCpf());
-			entity.setIncome(dto.getIncome());
-			entity.setBirthDate(dto.getBirthDate());
-			entity.setChildren(dto.getChildren());
+			copyDtoToEntity(dto, entity);
 			entity = repository.save(entity);
 			return new ClientDTO(entity);
 		}
@@ -62,6 +57,27 @@ public class ClientService {
 			throw new NotFoundEntityException("Id não encontrado!");
 						
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+		    repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new NotFoundEntityException("Id não encontrado! " + id);			
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataBaseException("Integridade violada!");
+		}	
+	}
+	
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 
 }
