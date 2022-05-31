4c640cb3cdacf9d808b09c23e1417c68da26fc63
Andev <andremoraes930@gmail.com>
2022-05-31 08:58:45 -0300

    Modified class exceptions

--- backend/src/main/java/com/eduardoandre/crudDeliver/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/eduardoandre/crudDeliver/resources/exceptions/ResourceExceptionHandler.java
@@ -6,21 +6,36 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.eduardoandre.crudDeliver.services.exceptions.DataBaseException;
 import com.eduardoandre.crudDeliver.services.exceptions.NotFoundEntityException;
 
+@ControllerAdvice
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(NotFoundEntityException.class)
 	public ResponseEntity<StandardError> entityNotFond(NotFoundEntityException e, HttpServletRequest request) {
-
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError error = new StandardError();
 		error.setTimestamp(Instant.now());
-		error.setStatus(HttpStatus.NOT_FOUND.value());
-		error.setError("Pesquisa não encontrada");
+		error.setStatus(status.value());
+		error.setError("Pesquisa não encontrada!");
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Pesquisa não encontrada!");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
 	}
 }
